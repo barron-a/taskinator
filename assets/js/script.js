2d4a57830b2310bd6700dafc8eeb8b0abfc8a5c0
Adam Barron <adambarron@me.com>
2020-10-05 23:59:31 -0700

    added functionality to capture form values and render them on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,20 @@ var createTaskHandler = function(event) {
 
     event.preventDefault();
 
-    var listItenEl = document.createElement("li");
-    listItenEl.className = "task-item";
-    listItenEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItenEl);
-    console.log(event);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", createTaskHandler);
