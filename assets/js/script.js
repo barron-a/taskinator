99d91c9a9f3873b6d2c119dd5f28f25bee88be10
Adam Barron <adambarron@me.com>
2020-10-05 23:40:22 -0700

    modified script.js to interact with form and prevent default (refresh) event behavior

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItenEl = document.createElement("li");
     listItenEl.className = "task-item";
     listItenEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItenEl);
+    console.log(event);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
